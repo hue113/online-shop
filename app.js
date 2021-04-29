123c092187d08ff24427509078ad5096c3596f05
Hue Pham <huepham113@gmail.com>
2021-04-29 02:52:07 -0400

    deployment

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const userRouter = require("./routes/userRoutes");
 const orderRouter = require("./routes/orderRoutes");
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 // app.options("*", cors());
 // app.use(
 //   cors({
@@ -29,16 +29,16 @@ const app = express();
 //   })
 // );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
 
-  next();
-});
+//   next();
+// });
 
 // Parse Cookie header
 app.use(cookieParser());
